6ff2c53385fbefc2026e43af731101e3d510466b
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2024-05-04 00:08:31 +0530

    Update promises.js

--- promises.js
+++ promises.js
@@ -1,3 +1,6 @@
+// Promises are a better way to write functions with callbacks
+//being one of the methods to make the code or asyncronus
+// we use promises to do task while some task being on queue in APIs
 function myFirstTimeOut(duration) {
   let p = new promise(function (resolve) {
     setTimeout(resolve, 1000);
