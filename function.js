83282d428046110a09a2c07784dd6e0a0c2f13b3
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2024-06-07 00:10:42 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+//The described function takes two values a and b, then logs the addition which is stored in Sum
 function findSum(a , b){
 Sum = a+b;
   console.log("The sum of the two numbers is:", Sum);
